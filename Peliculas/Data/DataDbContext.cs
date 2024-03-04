8e3a480c87f647e26468bea33e3d0db02812f24f
Oscar Nava <111458908+Oscar-RNS@users.noreply.github.com>
2024-03-04 00:41:59 -0600

    Movies - creating the migration: category

---
+++ Peliculas/Data/DataDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Peliculas.Models;
+
+namespace Peliculas.Data
+{
+    public class DataDbContext : DbContext
+    {
+     public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) { }
+
+     public DbSet<Category> Category { get; set; }
+
+    }
+ 
+}
---
+++ Peliculas/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Peliculas.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? NameCategory { get; set; }
+
+        public DateTime CreatedAt{ get; set; }
+    }
+}
